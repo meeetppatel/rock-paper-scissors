9598536de82af595b8ea22065d8025f9e502a470
ptlmeet27 <20124040@nuv.ac.in>
2022-06-19 04:04:07 +0530

    add

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 
+const buttons = document.querySelectorAll('input')
+const playagainbtn = document.querySelector('button')
+playagainbtn.disabled = true;
+document.getElementById("playagn").style.visibility = "hidden"
+
+
 function computerchoice() {
     let options = ['rock', 'paper', 'scissors'];
     let computerchoice = options[Math.floor(Math.random() * options.length)];
@@ -6,13 +12,22 @@ function computerchoice() {
     return computerchoice;
 }
 
-function playerchoice(){
-    let playerchoice = prompt('rock/paper/scissors');
-    console.log(`player's choice "${playerchoice}"`);
-    return playerchoice;
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
 }
 
-function playRound(playerSelection, computerSelection) {
+// function playerchoice(yourchoice){
+//     // let playerchoice = prompt('rock/paper/scissors');
+//     console.log(`player's choice "${yourchoice}"`);
+//     return yourchoice;
+// }
+
+function playRound(playerSelection) {
+    let computerSelection = computerchoice()
+
+
     if (playerSelection === 'rock' && computerSelection === 'rock') {
         console.log('Tie');
     }
@@ -47,35 +62,86 @@ function playRound(playerSelection, computerSelection) {
         return true;
     }
     else{
-        console.log("Illegal Choice");
+        console.log("invalid Choice");
         return -1;
     }
 }
 
 
-function game(){
-    let playerwincount = 0;
-    let compwincount = 0;
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = playerchoice();
-        const computerSelection = computerchoice();
-        if(playRound(playerSelection, computerSelection) === -1){
-            break;
-        }
-        else if (playRound(playerSelection, computerSelection)=== true) {
-            playerwincount++;
-        }
-        else if (playRound(playerSelection, computerSelection)=== false){
-            compwincount++;
-        }
-    }
-    // document.querySelector('#your_score').textContent = playerwincount;
-    // document.querySelector('#comp_score').textContent = compwincount;
+// function game(playerSelection){
+//     let playerwincount = 0;
+//     let compwincount = 0;
+//     // for (let i = 0; i < 5; i++) {
+        
+//     //     const computerSelection = computerchoice();
+//     //     if(playRound(playerSelection, computerSelection) === -1){
+//     //         break;
+//     //     }
+//     //     else if (playRound(playerSelection, computerSelection)=== true) {
+//     //         playerwincount++;
+//     //     }
+//     //     else if (playRound(playerSelection, computerSelection)=== false){
+//     //         compwincount++;
+//     //     }
+//     // }
+//     // document.querySelector('#your_score').textContent = playerwincount;
+//     // document.querySelector('#comp_score').textContent = compwincount;
+    
+//     if(playerwincount>compwincount){
+//         console.log(`You Won the match! with ${playerwincount}-${compwincount} `);
+//     }else{
+//         console.log(`You Lost the match! with ${playerwincount}-${compwincount}`);
+//     }
+// }
 
-    if(playerwincount>compwincount){
-        console.log(`You Won the match! with ${playerwincount}-${compwincount} `);
-    }else{
-        console.log(`You Lost the match! with ${playerwincount}-${compwincount}`);
+
+let playerwincount = 0;
+let compwincount = 0;
+
+
+function game(urchoice){
+    let result = ""
+    if(playRound(urchoice) === true){
+        playerwincount++;
+        document.querySelector('#your_score').textContent =playerwincount;
+        document.querySelector('#round_result').textContent = "You WON!";
+    }
+    else if(playRound(urchoice) === false){
+        compwincount++;
+        document.querySelector('#comp_score').textContent=compwincount;
+        document.querySelector('#round_result').textContent = "You Lost!";
+    }
+    else{
+        document.querySelector('#round_result').textContent = "It's a TIE!";
+    }
+    console.log(playerwincount, compwincount);
+    
+    if (playerwincount == 5){
+        result+= 'You won the game!! click the button to play again'
+        disableButtons()
+        document.querySelector('#result').textContent = result;
+        playagainbtn.disabled = false;
+        document.getElementById("playagn").style.visibility = "visible";
+    }
+    else if (compwincount == 5){
+        result += 'You lost the game!! click the button to play again'
+        disableButtons()
+        document.querySelector('#result').textContent = result;
+        playagainbtn.disabled = false;
+        document.getElementById("playagn").style.visibility = "visible";
     }
 }
-game();
+
+
+function playagain(){
+    window.location.reload();
+}
+
+
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        let playerchoice = button.value;
+        console.log(`player's choice${button.value}`);
+        game(playerchoice);
+    })
+})
